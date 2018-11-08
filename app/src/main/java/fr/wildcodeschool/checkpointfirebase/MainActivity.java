ddf152b9ab46aab01c9ca33c75f0dad8eccfabe2
Bastien Krettly <bastien_krettly@wilder.school>
2018-11-08 15:23:48 +0100

    init project

---
+++ app/src/main/java/fr/wildcodeschool/checkpointfirebase/MainActivity.java
@@ -0,0 +1,46 @@
+package fr.wildcodeschool.checkpointfirebase;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "DEBUG_FIREBASE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView firebaseTest = findViewById(R.id.firebase_test);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String value = dataSnapshot.getValue(String.class);
+                    firebaseTest.setText(value);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG, "onCancelled: " + databaseError.getMessage());
+            }
+        });
+
+        // TODO : modifier la valeur de la chaîne dans String.xml
+        String firebaseAccount = getString(R.string.firebase_node);
+    }
+}
